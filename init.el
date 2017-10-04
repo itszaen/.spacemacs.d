afec1420e47355c0a9c2271a07e9edf3bf2a9ca6
zaen323 <chouzaen2002@gmail.com>
2017-10-04 15:32:00 +0900

    see diff

--- init.el
+++ init.el
@@ -465,14 +465,14 @@
 
 
 ;;** Custom Bindings
-  (spacemacs/declare-prefix "o" "Custom")
+(spacemacs/declare-prefix "o" "Custom")
 
 ;;*** Switch to Org-agenda buffer
-  (defun Org-Agenda-buffer()
-    (interactive)
-    (switch-to-buffer "*Org Agenda*")
-    )
-  (spacemacs/set-leader-keys (kbd "SPC o a") 'Org-Agenda-buffer)
+(defun Org-Agenda-buffer()
+  (interactive)
+  (switch-to-buffer "*Org Agenda*")
+  )
+(spacemacs/set-leader-keys (kbd "SPC o a") 'Org-Agenda-buffer)
 
 ;;*** Org-agenda with todo files
 (defun Org-Agenda-Setup()
@@ -486,30 +486,37 @@
   )
 (spacemacs/set-leader-keys (kbd "SPC o A") 'Org-Agenda-Setup)
 ;;*** Org-TODO files
-  (spacemacs/declare-prefix "o t" "Org-files")
-  (defun TODOs ()
-    (interactive)
-    (find-file "~/Google Drive/Org/TODOs.org")
-    )
-  (defun Routines ()
-    (interactive)
-    (find-file "~/Google Drive/Org/Routines.org"))
-  (defun Timetable ()
-    (interactive)
-    (find-file "~/Google Drive/Org/Timetable.org"))
-  (defun Archive ()
-    (interactive)
-    (find-file "~/Google Drive/Org/Archive.org"))
-  (defun Projects ()
-    (interactive)
-    (find-file "~/Google Drive/Org/Projects.org"))
+(spacemacs/declare-prefix "o t" "Org-files")
+(defun TODOs ()
+  (interactive)
+  (find-file "~/Google Drive/Org/TODOs.org")
+  )
+(defun Routines ()
+  (interactive)
+  (find-file "~/Google Drive/Org/Routines.org"))
+(defun Timetable ()
+  (interactive)
+  (find-file "~/Google Drive/Org/Timetable.org"))
+(defun Archive ()
+  (interactive)
+  (find-file "~/Google Drive/Org/Archive.org"))
+(defun Projects ()
+  (interactive)
+  (find-file "~/Google Drive/Org/Projects.org"))
+(defun Study ()
+  (interactive)
+  (find-file "~/Google Drive/Org/Study.org"))
+(defun Fun ()
+  (interacrive)
+  (find-file "~/Google Drive/Org/Fun.org"))
 
   (spacemacs/set-leader-keys (kbd "SPC o t t") 'TODOs)
   (spacemacs/set-leader-keys (kbd "SPC o t r") 'Routines)
   (spacemacs/set-leader-keys (kbd "SPC o t T") 'Timetable)
   (spacemacs/set-leader-keys (kbd "SPC o t a") 'Archive)
   (spacemacs/set-leader-keys (kbd "SPC o t p") 'Projects)
-
+  (spacemacs/set-leader-keys (kbd "SPC o t s") 'Study)
+  (spacemacs/set-leader-keys (kbd "SPC o t f") 'Fun)
 ;;*** Notebooks
   (spacemacs/declare-prefix "o n" "Notebook")
   (spacemacs/declare-prefix "o n h" "History")
@@ -722,6 +729,7 @@
   (setq org-catch-invisible-edits t)
   (setq org-startup-folded 'content)
   (setq org-use-speed-commands t)
+  (setq org-pretty-entities t)
   (setq org-archive-location "~/Google Drive/Org/Archive.org::")
   (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
 
@@ -742,7 +750,7 @@
           '(("t" "Task" entry (file+headline "~/Google Drive/Org/TODOs.org" "Tasks")
              "** TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"today\"))\n %^{Effort}p \n")
             ("m" "Meeting" entry (file+headline "~/Google Drive/Org/TODOs.org" "Meetings")
-             "** MEETING %?\n %u\n %a %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"today\"))")
+             "** TODO %?\n %u\n %a %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"today\"))")
             ("j" "Journal" entry (file+datetree "~/Google Drive/Org/Journal.org")
              "** %?\nEntered on %U\n  %i\n  %a")
             ("P" "pull-request-review" entry (file+headline "~/Google Drive/Org/TODOs.org" "Pull Requests")
@@ -750,7 +758,7 @@
             ("p" "process-soon" entry (file+headline "~/Google Drive/Org/TODOs.org" "Email")
              "** TODO %a %?\nDEADLINE: %(org-insert-time-stamp (org-read-date nil t \"+2d\"))")
             ("w" "wait-for-reply" entry (file+headline "~/Google Drive/Org/TODOs.org" "Email")
-             "** WAIT %u %a %?\n")
+             "** WAITING %u %a %?\n")
             ("r" "redmine-issue" entry (file+headline "~/Google Drive/Org/TODOs.org" "Redmine Issue")
              "** TODO %a %?\nDEADLINE: %(org-insert-time-stamp (org-read-date nil t \"+2d\"))")
             ))
@@ -765,6 +773,8 @@
                           "~/Google Drive/Org/Projects.org"
                           "~/Google Drive/Org/Notes.org"
                           "~/Google Drive/Org/Timetable.org"
+                          "~/Google Drive/Org/Fun.org"
+                          "~/Google Drive/Org/Study.org"
                           ))
 
   (setq org-agenda-time-grid
@@ -1181,9 +1191,6 @@ This function is called at the very end of Spacemacs initialization."
     (name old-name general-category canonical-combining-class bidi-class decomposition decimal-digit-value digit-value numeric-value mirrored iso-10646-comment uppercase lowercase titlecase)))
  '(evil-want-Y-yank-to-eol t)
  '(initial-buffer-choice (quote org-super-zaen-view-startup))
- '(org-agenda-files
-   (quote
-    ("~/Google Drive/Org/TODOs.org" "~/Google Drive/Org/Routines.org" "~/Google Drive/Org/Projects.org" "~/Google Drive/Org/Notes.org" "~/Google Drive/Org/Timetable.org")))
  '(package-selected-packages
    (quote
     (beacon org-web-tools org-agenda-property plantuml-mode google-maps org-protocol-capture-html pug-mode inflections white-sand-theme symon string-inflection ruby-refactor test-simple loc-changes load-relative password-generator org-journal impatient-mode helm-purpose window-purpose imenu-list godoctor go-rename flycheck-bashate exotica-theme editorconfig company-php company-lua cmake-ide levenshtein browse-at-remote yascroll org-super-agenda org-redmine org-alert ac-php pyvenv mu4e-alert hy-mode geiser magit yasnippet company spaceline-all-the-icons github-search company-go markdown-mode magit-popup git-commit alert slime web-mode rebecca-theme realgud pyim pyenv-mode projectile-rails orgit org-brain mmm-mode magithub ghub+ apiwrap ghub live-py-mode evil-org evil-lion esh-help alchemist ac-php-core flycheck multiple-cursors org-projectile-helm org-category-capture minibuffer-line gitter per-buffer-theme load-theme-buffer-local color-theme-buffer-local color-theme emms ox-twbs ox-gfm org-notebook ox-reveal erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus company-emoji elfeed-goodies elfeed-org elfeed restclient epc wakatime-mode typit mmt sudoku spray slack emojify circe oauth2 ranger pangu-spacing pandoc-mode pacmacs ox-pandoc insert-shebang fish-mode find-by-pinyin-dired company-shell chinese-pyim chinese-pyim-basedict ace-pinyin pinyinlib 2048-game pdf-tools tablist ledger-mode gmail-message-mode ham-mode html-to-markdown flymd flycheck-ledger edit-server zonokai-theme zenburn-theme zen-and-art-theme underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme toml-mode tao-theme tangotango-theme tango-plus-theme tango-2-theme systemd sunny-day-theme sublime-themes subatomic256-theme subatomic-theme sql-indent spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme seti-theme reverse-theme rainbow-mode rainbow-identifiers railscasts-theme racer purple-haze-theme professional-theme planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme pastels-on-dark-theme organic-green-theme omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme noctilux-theme niflheim-theme naquadah-theme mustang-theme monokai-theme monochrome-theme molokai-theme moe-theme minimal-theme material-theme majapahit-theme madhat2r-theme lush-theme light-soap-theme jbeans-theme jazz-theme ir-black-theme inkpot-theme heroku-theme hemisu-theme hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme gandalf-theme flycheck-rust seq flatui-theme flatland-theme firebelly-theme farmhouse-theme espresso-theme ein deferred websocket dracula-theme django-theme darktooth-theme autothemer darkokai-theme darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-identifiers-mode clues-theme cherry-blossom-theme cargo rust-mode busybee-theme bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes afternoon-theme autotetris-mode persp-projectile twittering-mode github-browse-file ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))

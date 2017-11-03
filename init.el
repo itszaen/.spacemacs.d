2b9bc70998e948537ef80982171f629eb5fce6f1
zaen323 <chouzaen2002@gmail.com>
2017-11-03 22:09:33 +0900

    latex

--- init.el
+++ init.el
@@ -20,7 +20,11 @@
      emacs-lisp
      common-lisp
      scheme
-     latex
+     (latex             :variables
+                        latex-build-command "LaTeX"
+                        latex-enable-auto-fill t
+                        latex-enable-folding t
+                        latex-enable-magic nil)
      markdown
      go
      elixir
@@ -870,8 +874,17 @@
                                  :date today
                                  :todo "TODAY"
                                  :scheduled today
-                                 :order 1
-                                 )))))
+                                 :order 1)
+                          (:name "Due Today"
+                                 :deadline today
+                                 :order 2)
+                          (:name "Overdue"
+                                 :deadline past
+                                 :order 3)
+                          (:name "Due Soon"
+                                 :deadline future
+                                 :order 4)
+                          ))))
            (alltodo "" ((org-agenda-overriding-header "")
                         (org-agenda-property-list '("LOCATION" "TEACHER") )
                         (org-agenda-property-position 'where-it-fits)
@@ -883,19 +896,10 @@
                            (:name "NEXT TO DO"
                                   :todo "NEXT"
                                   :order 1)
-                           (:name "Due Today"
-                                  :deadline today
-                                  :order 2)
                            (:name "Important"
                                   :tag "Important"
                                   :priority "A"
                                   :order 6)
-                           (:name "Overdue"
-                                  :deadline past
-                                  :order 7)
-                           (:name "Due Soon"
-                                  :deadline future
-                                  :order 8)
                            (:name "Waiting"
                                   :todo "WAITING"
                                   :order 9)
@@ -974,6 +978,17 @@
        )
 
   ) ; with-eval-after-load 'org
+;;*** TeX
+(add-hook 'doc-view-mode-hook 'auto-revert-mode)
+(setq TeX-view-program-list
+      '(("Okular" "okular %o")
+        ("Chrome" "chromium %o")
+        ("pdf-tools" "TeX-pdf-tools-sync-view")))
+(setq TeX-view-program-selection
+      '((output-pdf "pdf-tools")))
+(setq TeX-source-correlate-mode t)
+(setq TeX-source-correlate-start-server t)
+(setq TeX-source-correlate-method 'synctex)
 ;;*** Helm
 (with-eval-after-load 'helm
 (define-key helm-buffer-map (kbd "C-c j") 'mozc-mode)
@@ -1262,7 +1277,7 @@ This function is called at the very end of Spacemacs initialization."
  '(initial-buffer-choice (quote org-super-zaen-view-startup))
  '(package-selected-packages
    (quote
-    (youdao-dictionary names chinese-word-at-point fcitx zonokai-theme zenburn-theme zen-and-art-theme yascroll yapfify xterm-color ws-butler winum white-sand-theme which-key web-mode web-beautify wakatime-mode volatile-highlights uuidgen use-package unfill underwater-theme ujelly-theme typit twittering-mode twilight-theme twilight-bright-theme twilight-anti-bright-theme toxi-theme toml-mode toc-org tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit systemd symon sunny-day-theme sudoku sublime-themes subatomic256-theme subatomic-theme string-inflection sql-indent spray spaceline-all-the-icons spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slime-company slim-mode slack shell-pop seti-theme scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor rubocop rspec-mode robe reverse-theme restart-emacs rebecca-theme realgud rbenv ranger rainbow-mode rainbow-identifiers rainbow-delimiters railscasts-theme racer pyvenv pytest pyim pyenv-mode py-isort purple-haze-theme pug-mode projectile-rails professional-theme powershell popwin plantuml-mode planet-theme pip-requirements phpunit phpcbf php-extras php-auto-yasnippets phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode per-buffer-theme pdf-tools password-generator paradox pangu-spacing pandoc-mode pacmacs ox-twbs ox-reveal ox-pandoc ox-gfm orgit organic-green-theme org-web-tools org-super-agenda org-redmine org-protocol-capture-html org-projectile-helm org-present org-pomodoro org-notebook org-journal org-download org-bullets org-brain org-alert org-agenda-property open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-elixir noctilux-theme neotree naquadah-theme mwim mustang-theme multi-term mu4e-maildirs-extension mu4e-alert move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minitest minimal-theme minibuffer-line material-theme markdown-toc majapahit-theme magit-gitflow magit-gh-pulls madhat2r-theme lush-theme lorem-ipsum load-theme-buffer-local livid-mode live-py-mode linum-relative link-hint light-soap-theme less-css-mode ledger-mode json-mode js2-refactor js-doc jbeans-theme jazz-theme ir-black-theme insert-shebang inkpot-theme info+ indent-guide impatient-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate google-maps golden-ratio godoctor go-rename go-guru go-eldoc gnuplot gmail-message-mode gitter github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md geiser gandalf-theme fuzzy fsharp-mode flyspell-correct-helm flymd flycheck-rust flycheck-pos-tip flycheck-mix flycheck-ledger flycheck-credo flycheck-bashate flx-ido flatui-theme flatland-theme fish-mode find-by-pinyin-dired fill-column-indicator feature-mode farmhouse-theme fancy-battery eyebrowse expand-region exotica-theme exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus emms emmet-mode elisp-slime-nav ein editorconfig edit-server dumb-jump drupal-mode dracula-theme django-theme disaster diff-hl dfraw-mode deft define-word darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode cyberpunk-theme concurrent company-web company-tern company-statistics company-shell company-php company-lua company-go company-emoji company-c-headers company-auctex company-anaconda common-lisp-snippets column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-buffer-local color-identifiers-mode coffee-mode cnfonts cmake-mode cmake-ide clues-theme clean-aindent-mode clang-format chruby cherry-blossom-theme cargo busybee-theme bundler bubbleberry-theme browse-at-remote birds-of-paradise-plus-theme beacon badwolf-theme auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile auctex-latexmk apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes alchemist aggressive-indent afternoon-theme adaptive-wrap ace-window ace-pinyin ace-link ace-jump-helm-line ac-php ac-mozc ac-ispell 2048-game)))
+    (mmt slime circe oauth2 test-simple loc-changes youdao-dictionary names chinese-word-at-point fcitx zonokai-theme zenburn-theme zen-and-art-theme yascroll yapfify xterm-color ws-butler winum white-sand-theme which-key web-mode web-beautify wakatime-mode volatile-highlights uuidgen use-package unfill underwater-theme ujelly-theme typit twittering-mode twilight-theme twilight-bright-theme twilight-anti-bright-theme toxi-theme toml-mode toc-org tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit systemd symon sunny-day-theme sudoku sublime-themes subatomic256-theme subatomic-theme string-inflection sql-indent spray spaceline-all-the-icons spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slime-company slim-mode slack shell-pop seti-theme scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor rubocop rspec-mode robe reverse-theme restart-emacs rebecca-theme realgud rbenv ranger rainbow-mode rainbow-identifiers rainbow-delimiters railscasts-theme racer pyvenv pytest pyim pyenv-mode py-isort purple-haze-theme pug-mode projectile-rails professional-theme powershell popwin plantuml-mode planet-theme pip-requirements phpunit phpcbf php-extras php-auto-yasnippets phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode per-buffer-theme pdf-tools password-generator paradox pangu-spacing pandoc-mode pacmacs ox-twbs ox-reveal ox-pandoc ox-gfm orgit organic-green-theme org-web-tools org-super-agenda org-redmine org-protocol-capture-html org-projectile-helm org-present org-pomodoro org-notebook org-journal org-download org-bullets org-brain org-alert org-agenda-property open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-elixir noctilux-theme neotree naquadah-theme mwim mustang-theme multi-term mu4e-maildirs-extension mu4e-alert move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minitest minimal-theme minibuffer-line material-theme markdown-toc majapahit-theme magit-gitflow magit-gh-pulls madhat2r-theme lush-theme lorem-ipsum load-theme-buffer-local livid-mode live-py-mode linum-relative link-hint light-soap-theme less-css-mode ledger-mode json-mode js2-refactor js-doc jbeans-theme jazz-theme ir-black-theme insert-shebang inkpot-theme info+ indent-guide impatient-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate google-maps golden-ratio godoctor go-rename go-guru go-eldoc gnuplot gmail-message-mode gitter github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md geiser gandalf-theme fuzzy fsharp-mode flyspell-correct-helm flymd flycheck-rust flycheck-pos-tip flycheck-mix flycheck-ledger flycheck-credo flycheck-bashate flx-ido flatui-theme flatland-theme fish-mode find-by-pinyin-dired fill-column-indicator feature-mode farmhouse-theme fancy-battery eyebrowse expand-region exotica-theme exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help erc-yt erc-view-log erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus emms emmet-mode elisp-slime-nav ein editorconfig edit-server dumb-jump drupal-mode dracula-theme django-theme disaster diff-hl dfraw-mode deft define-word darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode cyberpunk-theme concurrent company-web company-tern company-statistics company-shell company-php company-lua company-go company-emoji company-c-headers company-auctex company-anaconda common-lisp-snippets column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-buffer-local color-identifiers-mode coffee-mode cnfonts cmake-mode cmake-ide clues-theme clean-aindent-mode clang-format chruby cherry-blossom-theme cargo busybee-theme bundler bubbleberry-theme browse-at-remote birds-of-paradise-plus-theme beacon badwolf-theme auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile auctex-latexmk apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes alchemist aggressive-indent afternoon-theme adaptive-wrap ace-window ace-pinyin ace-link ace-jump-helm-line ac-php ac-mozc ac-ispell 2048-game)))
  '(paradox-github-token t)
  '(wakatime-python-bin nil))
 (custom-set-faces
